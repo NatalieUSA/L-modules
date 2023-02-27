971ab75a163b51dafacdc22b642d272682867ff1
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-02-28 01:21:35 +0200

    form-components + add files MyBooksLife

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import Counter from './R-m2/Counter/Counter';
 import LessonsTitle from './shared/components/LessonsTitle/LessonsTitle';
 import Dropdown from './R-m2/Dropdown/Dropdown';
 import ColorPicker from './R-m2/ColorPicker/ColorPicker';
+import MyBooksLife from './modules/MyBooks-L-3-1/MyBooksLife';
 
+import MyBooksComponent from './modules/MyBooks-components-example/MyBooksComponents';
 const colorPickerOptions = [
   { label: 'red', color: '#ff2803' },
   { label: 'green', color: '#10ec18' },
@@ -16,12 +18,17 @@ const colorPickerOptions = [
 export const App = () => {
   return (
     <div className="App">
+      <LessonsTitle>Lyamzin. Modul 3.1 Жизненный цикл</LessonsTitle>
+      <MyBooksLife />
+      <LessonsTitle>Lyamzin. Modul 2.3 form</LessonsTitle>
+      <MyBooks />
+      <LessonsTitle> form-Component</LessonsTitle>
+      <MyBooksComponent />
+
       <LessonsTitle>Repeta. Modul 2.1 Class</LessonsTitle>
       <Counter />
       <Dropdown />
       <ColorPicker options={colorPickerOptions} />
-      <LessonsTitle>Lyamzin. Modul 2.3 form</LessonsTitle>
-      <MyBooks />
     </div>
   );
 };
